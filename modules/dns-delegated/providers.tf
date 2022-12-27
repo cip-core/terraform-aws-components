8a80c0d7789794ee28371861bcde500f647b6fe8
Patrice Lachance <patlachance@gmail.com>
2022-12-27 16:55:07 +0100

    DON'T USE THIS BRANCH, HISTORY WILL BE REWRITTEN!

--- modules/dns-delegated/providers.tf
+++ modules/dns-delegated/providers.tf
@@ -28,8 +28,12 @@ provider "aws" {
 }
 
 module "iam_roles" {
-  source  = "../account-map/modules/iam-roles"
-  context = module.this.context
+  source = "../cip-central-account/modules/iam-roles"
+  stage  = var.stage
+  region = var.region
+  tfstate_bucket_environment_name = var.tfstate_bucket_environment_name
+  context     = module.this.context
+  tfstate_account_id = var.tfstate_account_id
 }
 
 variable "import_profile_name" {
